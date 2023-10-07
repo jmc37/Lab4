a7f82fdc896c552ac13e0c9e8bef5266fbc87556
Joseph Chan <josephchan36@gmail.com>
2023-10-07 16:34:07 -0700

    Updated create item and find item functions

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const endPointRoot = "http://localhost:8080/"
+const endPointRoot = "http://localhost:8080/";
+
 http.createServer(function (req, res) {
   let q = url.parse(req.url, true);
   let filename = "." + q.pathname;
@@ -17,11 +18,19 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 function createItem(event) {
+    event.preventDefault();
+    let term = document.getElementById("new-term").value;
+    let definition = document.getElementById("definition").value;
+    let data = {
+      "term": term,
+      "definition": definition
+    }
+    let jsonData = JSON.stringify(data)
     let resource = "create/"
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", endPointRoot + resource, true);
+    xhttp.open("POST", 'http://localhost:8888/', true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(event);
+    xhttp.send(jsonData);
     xhttp.onreadystatechange =function ()  {
         if(this.readyState == 4 && this.status == 200){
             document.getElementById("result").innerHTML =this.responseText;
@@ -29,10 +38,12 @@ function createItem(event) {
     }
 }
 
-function getDefaultAutoSelectFamily() {
+function getItem(event) {
+    event.preventDefault();
     const xhttp = new XMLHttpRequest();
+    let term = document.getElementById("search-term").value;
     let resource = 'get/'
-    xhttp.open("GET", endPointRoot + resource, true);
+    xhttp.open("GET", 'http://localhost:8888/' + `?term=${term}`, true);
     xhttp.send();
     xhttp.onreadystatechange =function ()  {
         if(this.readyState == 4 && this.status == 200){
