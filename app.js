c49fc23a2e131c7bcefa1c3a497c0755e71d9002
Joseph Chan <josephchan36@gmail.com>
2023-10-08 11:49:42 -0700

    updated routes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+const create_route = 'http://localhost:8888/create'
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const endPointRoot = "http://localhost:8080/";
-const CREATE_ITEM_ROUTE = "create/"
 
 
 http.createServer(function (req, res) {
@@ -20,23 +19,23 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 function createItem(event) {
+    event.preventDefault();
     let regex = /^[a-zA-Z]+$/;
     let definitionRegex = /^[a-zA-Z\s]+$/;
-    event.preventDefault();
     let term = document.getElementById("new-term").value;
     let definition = document.getElementById("definition").value;
 
     if (!regex.test(term) || !definitionRegex.test(definition)) {
         alert("Both term and definition must be valid strings (letters only).");
         return;
-    }
+    } 
     let data = {
       "term": term,
       "definition": definition
     }
     let jsonData = JSON.stringify(data)
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", 'http://localhost:8888/', true);
+    xhttp.open("POST", create_route, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(jsonData);
     xhttp.onreadystatechange =function ()  {
@@ -55,8 +54,7 @@ function getItem(event) {
       return;
   }
     const xhttp = new XMLHttpRequest();
-    let resource = 'get/'
-    xhttp.open("GET", 'http://localhost:8888/' + `?term=${term}`, true);
+    xhttp.open("GET", `http://localhost:8888/search/?term=${term}`, true);
     xhttp.send();
     xhttp.onreadystatechange =function ()  {
         if(this.readyState == 4 && this.status == 200){
