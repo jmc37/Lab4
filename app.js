95c7362720c1a963708c64016ae28e9b17f2cb94
Joseph Chan <josephchan36@gmail.com>
2023-10-08 16:29:58 -0700

    changed to static

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-const create_route = process.env.CREATE_ROUTE
-const search_route = process.env.SEARCH_ROUTE
-const page_redirect = process.env.PAGE_REDIRECT
+require('dotenv').config();
+const create_route = process.env.CREATE_ROUTE;
+const search_route = process.env.SEARCH_ROUTE;
+const page_redirect = process.env.PAGE_REDIRECT;
 const alert_create = "Both term and definition must be valid strings (letters only)."
 const alert_get = "Term must be valid strings (letters only)."
-const PORT = process.env.PORT || 8050;
 const http = require('http');
-const url = require('url');
-const fs = require('fs');
-
-
-http.createServer(function (req, res) {
-  let q = url.parse(req.url, true);
-  let pathname = q.pathname;
-  
-  // Handle the root URL ("/")
-  if (pathname === '/') {
-    // Redirect to search.html
-    res.writeHead(302, {
-      'Location': page_redirect
-    });
-    res.end();
-    return;
-  }
-  
-  let filename = "." + pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(PORT);
 
 function createItem(event) {
     event.preventDefault();
